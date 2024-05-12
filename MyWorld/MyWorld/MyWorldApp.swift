df48a0111a995b67d5970080aa030f845b8f856b
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2024-05-12 17:49:23 +0200

    Proyect creation

---
+++ MyWorld/MyWorld/MyWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorldApp.swift
+//  MyWorld
+//
+//  Created by José Luis Corral on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
