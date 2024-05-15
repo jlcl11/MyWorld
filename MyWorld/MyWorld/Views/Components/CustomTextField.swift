68101e899ee98c175d61a6ca6a2d5e8983a0799a
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2024-05-15 14:40:54 +0200

    Recording audio animation

--- MyWorld/MyWorld/Views/Components/CustomTextField.swift
+++ MyWorld/MyWorld/Views/Components/CustomTextField.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct CustomTextField: View {
     @Binding var searchText: String
+    @State private var isRecordingAudio: Bool = false
+    @State private var isHoldingButton = false
+    
     var body: some View {
         HStack {
             Image(systemName: "magnifyingglass")
                 .padding()
                 .foregroundStyle(.gray)
             
-            TextField("Search locations here", text: $searchText)
-                .padding(.vertical, 20)
-                .foregroundStyle(.gray)
+            if !isRecordingAudio {
+                TextField("Search locations here", text: $searchText)
+                    .padding(.vertical, 20)
+                    .foregroundStyle(.gray)
+            } else {
+                Spacer()
+                WavingBars()
+                WavingBars()
+                Spacer()
+            }
             
             Button{
+                isRecordingAudio.toggle()
                 
             }label: {
-                Image(systemName: "mic")
+                Image(systemName: isRecordingAudio ? "stop.circle.fill" : "mic")
                     .padding()
                     .tint(.gray)
-            }
+            }  .simultaneousGesture(LongPressGesture(minimumDuration: 0.2).onEnded { _ in
+                print("long press")
+                isRecordingAudio.toggle()
+                
+            })
         }
         .background(Color.gray.opacity(0.1))
         .cornerRadius(50)
